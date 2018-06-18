7862e0a2198991a7d7362484b0411b1a00b5b9f7
PrizracAnd <prizracand@gmail.com>
2018-06-18 18:00:46 +0300

    Initial commit

---
+++ app/src/main/java/demjanov/av/ru/rx11/MainActivity.java
@@ -0,0 +1,166 @@
+package demjanov.av.ru.rx11;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import rx.Observable;
+import rx.Observer;
+import rx.Scheduler;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+
+public class MainActivity extends AppCompatActivity {
+    final static String TAG_OBSERVER = "OBSERVER";
+
+    private boolean isObservable = true;
+//    private boolean isCompleted = false;
+
+    private List<String> stringList = new ArrayList<String>();
+
+    EditText editText;
+    TextView textView;
+
+
+    /////////////////////////////////////////////////////
+    // Method onCreate
+    ////////////////////////////////////////////////////
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+    /////////////////////////////////////////////////////
+    // Method onDestroy
+    ////////////////////////////////////////////////////
+    @Override
+    protected void onDestroy() {
+        isObservable = false;
+
+        super.onDestroy();
+    }
+
+    /////////////////////////////////////////////////////
+    // Method initializeEditText
+    ////////////////////////////////////////////////////
+    private void initializeEditText(){
+        editText = (EditText)findViewById(R.id.textEdit);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {      //FIXME --Add logic!!!
+                stringList.add(String.valueOf(s));
+            }
+        });
+    }
+
+
+    /////////////////////////////////////////////////////
+    // Method createObservable
+    ////////////////////////////////////////////////////
+    private Observable createObservable(final String str){
+        Observable.OnSubscribe<String> onSubscribe = new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                int i = 0;
+
+                while (isObservable && !subscriber.isUnsubscribed()) {
+                    if(i < stringList.size()) {
+                        if (subscriber.isUnsubscribed()) {
+                            return;
+                        }
+                        subscriber.onNext(stringList.get(i));
+                        i++;
+
+                        if(i > 200){
+                            recreateList();
+                            i = 0;
+                        }
+                    }
+                }
+
+                if(subscriber.isUnsubscribed()){
+                    return;
+                }
+                subscriber.onCompleted();
+            }
+        };
+
+        return Observable.create(onSubscribe)               //FIXME --fiend alternative for create
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+
+    }
+
+
+
+    /////////////////////////////////////////////////////
+    // Method createObserver
+    ////////////////////////////////////////////////////
+    private Observer createObserver(){
+//        isCompleted = false;
+
+        return new Observer<String>() {
+            @Override
+            public void onCompleted() {
+                Log.d(TAG_OBSERVER, "onCompleted");
+//                isCompleted = true;
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG_OBSERVER, "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.d(TAG_OBSERVER, "onNext: " + s);
+                updateText(s);
+            }
+
+        };
+    }
+
+
+    /////////////////////////////////////////////////////
+    // Method updateText
+    ////////////////////////////////////////////////////
+    private void updateText(String s) {
+        textView.setText(s);
+    }
+
+
+
+    /////////////////////////////////////////////////////
+    // Method for recreateList
+    ////////////////////////////////////////////////////
+    private void recreateList() {
+        stringList = new ArrayList<String>();
+    }
+
+
+
+}
